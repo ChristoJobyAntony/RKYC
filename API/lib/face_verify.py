2376e8ec06d16a747f01259153a572e508ca596e
zahransajid <zahransajid3@gmail.com>
2022-10-01 12:44:34 +0530

    Added docs, modified face_verify.py

--- API/lib/face_verify.py
+++ API/lib/face_verify.py
@@ -4,9 +4,21 @@ import numpy as np
 
 
 class Verifier():
-    def __init__(self) -> None:
-        pass
+    def __init__(self, threshold : float = 0.35) -> None:
+        """A helper class to hold the methods required for face authentication
+
+        Args:
+            threshold (float, optional): Threshold for similarity between faces. Defaults to 0.35.
+        """
+        self.threshold = threshold
+        
     def compare(self,compare_path : str, truth_path : str):
+        """Compares a face in given image against face in ground truth image and returns a score or None if distance is too far/no face detected.
+
+        Args:
+            compare_path (str): path to comparison image
+            truth_path (str): path to ground truth image
+        """
         frame = cv2.imread(compare_path)
         t_img = face_recognition.load_image_file(truth_path)
         face_encoding = face_recognition.face_encodings(t_img)[0]
@@ -17,5 +29,8 @@ class Verifier():
         face_encodings = face_recognition.face_encodings(rgb_small_frame, face_locations)
         for face_encoding in face_encodings:
             face_distances = face_recognition.face_distance(known_face_encodings, face_encoding)
-            print(face_distances)
+            if(face_distances):
+                return face_distances[0]
+            else:
+                return face_distances
         
