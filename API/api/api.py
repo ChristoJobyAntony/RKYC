59b25ca6e4ed1db8e30eb883591745c361a3b32b
zahransajid <zahransajid3@gmail.com>
2022-10-01 23:06:18 +0530

    Modified api.py

--- API/api/api.py
+++ API/api/api.py
@@ -41,7 +41,7 @@ def user_enroll_otp_issue (aadhaar_id: str, db:Session=Depends(get_db)) :
     return t
     
 
-@app.get("/user/enroll/otp/verify", response_model=schema.Result) 
+@app.post("/user/enroll/otp/verify", response_model=schema.Result) 
 def user_enroll_otp_verify (aadhaar_id: str, token:str,  otp:int, db:Session=Depends(get_db)) :
     t = curd.get_otp(db, token)
     if (not t) : 
@@ -56,7 +56,7 @@ def user_enroll_otp_verify (aadhaar_id: str, token:str,  otp:int, db:Session=Dep
     }
 
 @app.post("/user/enroll/register")
-async def read_root(aadhaar_id:str, otp_token: str, video: UploadFile = File(), db=Depends(get_db), audio: UploadFile=File()):
+async def user_enroll_register(aadhaar_id:str, otp_token: str, video: UploadFile = File(), db=Depends(get_db), audio: UploadFile=File()):
     otp_data = curd.get_otp(db,token=otp_token)
     if(otp_data.validated != True and otp_data.aadhaar_id == aadhaar_id):
         return HTTPException(status_code=498,detail="The given otp token is not validated")
@@ -79,3 +79,50 @@ async def read_root(aadhaar_id:str, otp_token: str, video: UploadFile = File(), 
 @app.get("/test")
 def delete (db=Depends(get_db)) :
     curd.delete_expired_otp(db)
+
+@app.post("/user/verify/otp/issue", response_model=schema.OTPBase)
+def user_enroll_otp_issue (aadhaar_id: str, db:Session=Depends(get_db)) :
+    # Verification OTP
+    if(curd.get_user(db, aadhaar_id=aadhaar_id) == None):
+        return HTTPException(status_code=400, detail="Invalid user")
+    t = curd.issue_otp(db, aadhaar_id, config.OTP_VERIFY)
+    t =  curd.get_otp(db, t)
+    print(t.timestamp)
+    return t
+
+@app.get("/user/verify/otp/verify", response_model=schema.Result) 
+def user_enroll_otp_verify (aadhaar_id: str, token:str,  otp:int, db:Session=Depends(get_db)) :
+    t = curd.get_otp(db, token)
+    if (not t) : 
+        raise HTTPException(status_code=400, detail="The token is expired or not found !")
+    if (t.aadhaar_id != aadhaar_id) :
+        raise HTTPException(status_code=400, detail="Invalid request")
+    if (otp != t.otp) :
+        raise HTTPException(status_code=400, detail="Invalid OTP")
+    curd.verify_otp(db, token)
+    return {
+        "result": True
+    }
+
+@app.post("/user/verify/verify")
+async def read_root(aadhaar_id:str, otp_token: str, video: UploadFile = File(), db=Depends(get_db), audio: UploadFile=File()):
+    otp_data = curd.get_otp(db,token=otp_token)
+    user_data = curd.get_user(db,aadhaar_id=aadhaar_id)
+    if(otp_data.validated != True and otp_data.aadhaar_id == aadhaar_id):
+        return HTTPException(status_code=498,detail="The given otp token is not validated")
+    if(user_data.is_enrolled != True):
+        return HTTPException(status_code=498,detail="The given user is not enrolled")
+    video_path = config.VIDEO_COMPARE_PATH + aadhaar_id + ".mp4"
+    audio_path = config.AUDIO_PATH + aadhaar_id + ".wav"
+    if video.content_type != "video/webm" :
+        raise HTTPException(status_code=400, detail="Invalid Video File")
+    if audio.content_type != "audio/wav" :
+        raise HTTPException(status_code=400, detail="Invalid Audio File")
+    with open(video_path, "wb+") as file_object:
+        shutil.copyfileobj(video.file, file_object) 
+    with open(audio_path, "wb+") as file_object : 
+        shutil.copyfileobj(audio.file, file_object)
+    voice_score = voiceEmbedder.compare(audio_path,aadhaar_id)
+    face_extractor = face_orient.FaceFile(video_path,save_path=config.FACE_COMPARE_PATH + aadhaar_id + ".png")
+    orientations = face_extractor.process_input()
+    return {}
